12e508bb1c3d975843ae4ef1ed7b4b1b6c975a68
ramyas13 <ramyashree1301@gmail.com>
2022-10-07 12:27:09 +0530

    commit

--- generics.cs
+++ generics.cs
@@ -9,21 +9,36 @@ namespace TE_CSharp
     internal class generics
     {
         // Declare Generic Delegate
-        public delegate T SampleDelegate<T>(T a,T b);
+        public delegate T SampleDelegate<T>(T a);
         class Area
         {
-            public int Square(int a,int b)
+            public int Square(int a)
             {
-                return a*b;
+                return a*a;
             }
-            public int Cube(int c,int d)
+            public double Square(double a)
             {
-                return 6*(c*d);
+                return a * a;
             }
-            public double Circle(double r,double pi)
+            public int Cube(int c)
+            {
+                return 6*(c*c);
+            }
+            public double Cube1(double c)
+            {
+                return 6 * (c * c);
+            }
+            public int Circle(int r)
+            {
+
+                return (int) 3.142 * (r * r);
+
+            }
+
+            public double Circle(double r)
             {
                
-                return pi*(r*r);
+                return 3.142*(r*r);
 
             }
  
@@ -34,15 +49,29 @@ namespace TE_CSharp
             {
                 Console.WriteLine("****Generic Delegate Example****");
                 Area m = new Area();
+                Console.WriteLine("-----------Area Using int Values-----------");
                 // Instantiate delegate with Square method using int
                 SampleDelegate<int> dlgt = new SampleDelegate<int>(m.Square);
-                Console.WriteLine("Area of the Square: " + dlgt(5,5));
+                Console.WriteLine("Area of the Square: " + dlgt(5));
                 // Instantiate delegate with Cube method
                 dlgt = m.Cube;
-                Console.WriteLine("Area of Cube: " + dlgt(9,9));
+                Console.WriteLine("Area of Cube: " + dlgt(9));
                 // Instantiate delegate with Circle method using double
-                SampleDelegate<double> dlgt1 = new SampleDelegate<double>(m.Circle);
-                Console.WriteLine("Area of the circle: " + dlgt1(5,3.14));
+                SampleDelegate<int> dlgt1 = new SampleDelegate<int>(m.Circle);
+                Console.WriteLine("Area of the circle: " + dlgt1(6));
+
+                Console.WriteLine("----------Area Using double Values---------");
+                // Instantiate delegate with Square method using int
+                SampleDelegate<double> dlgt3 = new SampleDelegate<double>(m.Square);
+                Console.WriteLine("Area of the Square: " + dlgt3(5.2));
+
+                // Instantiate delegate with Cube method
+                SampleDelegate<double> dlgt4 = new SampleDelegate<double>(m.Cube1);
+                Console.WriteLine("Area of the Cube: " + dlgt4(5.2));
+
+                // Instantiate delegate with Circle method using double
+                SampleDelegate<double> dlgt5 = new SampleDelegate<double>(m.Circle);
+                Console.WriteLine("Area of the circle: " + dlgt5(6.2));
 
             }
         }
